bde299e8d18e8c22bb7606d2c7b826000a955c39
zhoudi <364044599@qq.com>
2022-05-23 15:29:53 +0800

    add tag

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const { response } = require("express");
-
+const stageTagsListCn = require('./backdrops-tags-cn.json')
+const stageTagsListTw = require('./backdrops-tags-tw.json')
+const stageTagsListEn = require('./backdrops-tags-en.json')
+const stageList = require('./backdrops.json')
 const app = express();
 
 app.get("*", function (req, res, next) {
@@ -33,8 +36,17 @@ app.get("/internalapi/asset/*/get/", function (req, res) {
     res.end("no file " + url);
   }
 });
-
+var lanMap=[]
+lanMap['en']=stageTagsListEn
+lanMap['zh-cn']=stageTagsListCn
+lanMap['zh-tw']=stageTagsListTw
+app.get("/getStageTags", function (req, res) {
+  res.send(lanMap[req.query.language]);
+});
+app.get("/getStageList", function (req, res) {
+  res.send(stageList);
+});
 const port = 8603;
 app.listen(port,'127.0.0.1', function () {
-  console.log("sever listen to : " + port);
+  console.log("sever05231346 listen to : " + port);
 });
